d70cee3dad29033e9a843f032b7a2b2260ea7423
choijihaeSunny <jhchoisunny@gmail.com>
2024-04-01 14:38:37 +0900

    검사장비, 검사내역 테이블 생성문

---
+++ CREATE_TABLE_TB_MSUR_EQUICHECK.sql
@@ -0,0 +1,31 @@
+-- swmcp.tb_msur_equicheck definition
+
+CREATE TABLE `tb_msur_equicheck` (
+  `COMP_ID` varchar(10) NOT NULL COMMENT '사업장',
+  `EQUI_CODE` varchar(20) NOT NULL COMMENT '관리번호',
+  `CHECK_ITEM` varchar(10) NOT NULL COMMENT '점검항목',
+  `CYCLE` decimal(3,0) DEFAULT 0 COMMENT '점검주기(월)',
+  `FINAL_DATE` timestamp NULL DEFAULT current_timestamp() COMMENT '최종점검일',
+  `NEXT_DATE` timestamp NULL DEFAULT current_timestamp() COMMENT '차기점검일',
+  `CHECK_DEPT` varchar(10) DEFAULT NULL COMMENT '점검기관',
+  `CHECK_RESULT` varchar(10) DEFAULT NULL COMMENT '점검결과',
+  `CHECK_EMP_NO` varchar(10) DEFAULT NULL COMMENT '점검자',
+  `VALID_TEMP` decimal(10,0) DEFAULT 0 COMMENT '유효기간',
+  `CHECK_HIS` varchar(200) DEFAULT NULL COMMENT '점검내용',
+  `RESULT_HIS` varchar(200) DEFAULT NULL COMMENT '조치내용',
+  `ETC_RMK` varchar(200) DEFAULT NULL COMMENT '비고',
+  `FILE_NAME` varchar(100) DEFAULT NULL COMMENT '파일명',
+  `REAL_FILE_NAME` varchar(100) DEFAULT NULL COMMENT '실제파일명',
+  `TEMP1` varchar(10) DEFAULT NULL COMMENT '임시01',
+  `TEMP2` varchar(10) DEFAULT NULL COMMENT '임시02',
+  `TEMP3` varchar(10) DEFAULT NULL COMMENT '임시03',
+  `TEMP4` varchar(10) DEFAULT NULL COMMENT '임시04',
+  `TEMP5` varchar(10) DEFAULT NULL COMMENT '임시05',
+  `SYS_EMP_NO` varchar(10) NOT NULL COMMENT '등록사번',
+  `SYS_ID` varchar(30) NOT NULL COMMENT '등록ID',
+  `SYS_DATE` timestamp NULL DEFAULT current_timestamp() COMMENT '등록일자',
+  `UPD_EMP_NO` varchar(10) DEFAULT NULL COMMENT '수정사번',
+  `UPD_ID` varchar(30) DEFAULT NULL COMMENT '수정ID',
+  `UPD_DATE` timestamp NULL DEFAULT current_timestamp() COMMENT '수정일자',
+  PRIMARY KEY (`COMP_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='검사점검항목등록';
--- CREATE_TABLE_TB_RACK.sql
+++ CREATE_TABLE_TB_RACK.sql
@@ -1,15 +1,17 @@
+-- swmcp.tb_rack definition
+
 CREATE TABLE `tb_rack` (
   `COMP_ID` varchar(10) NOT NULL COMMENT '사업장코드',
-  `RACK_CODE` varchar(20) not null COMMENT '창고랙코드',
-  `RACK_NAME` varchar(30) not null COMMENT '창고랙이름',
-  `RACK_DIV` bigint(20) not null COMMENT '랙구분',
-  `FLOOR` varchar(3) not null COMMENT '층',
-  `ROOM` varchar(3)	not null COMMENT '칸',
-  `SPEC` bigint(20) not null COMMENT '용량',
-  `SIZE_R` decimal(10, 0) default 0 COMMENT '가로(cm)',
-  `SIZE_V` decimal(10, 0) default 0 COMMENT '세로(cm)',
-  `SIZE_H` decimal(10, 0) default 0 COMMENT '높이(cm)',
-  `RMK` varchar(100) default null COMMENT '비고',
+  `RACK_CODE` varchar(20) NOT NULL COMMENT '창고랙코드',
+  `RACK_NAME` varchar(30) NOT NULL COMMENT '창고랙이름',
+  `RACK_DIV` bigint(20) NOT NULL COMMENT '랙구분',
+  `FLOOR` varchar(3) NOT NULL COMMENT '층',
+  `ROOM` varchar(3) NOT NULL COMMENT '칸',
+  `SPEC` bigint(20) NOT NULL COMMENT '용량',
+  `SIZE_R` decimal(10,0) DEFAULT 0 COMMENT '가로(cm)',
+  `SIZE_V` decimal(10,0) DEFAULT 0 COMMENT '세로(cm)',
+  `SIZE_H` decimal(10,0) DEFAULT 0 COMMENT '높이(cm)',
+  `RMK` varchar(100) DEFAULT NULL COMMENT '비고',
   `TEMP1` varchar(10) DEFAULT NULL COMMENT '임시01',
   `TEMP2` varchar(10) DEFAULT NULL COMMENT '임시02',
   `TEMP3` varchar(10) DEFAULT NULL COMMENT '임시03',
@@ -18,8 +20,8 @@ CREATE TABLE `tb_rack` (
   `SYS_EMP_NO` varchar(10) NOT NULL COMMENT '등록사번',
   `SYS_ID` varchar(30) NOT NULL COMMENT '등록ID',
   `SYS_DATE` timestamp NULL DEFAULT current_timestamp() COMMENT '등록일자',
-  `UPD_EMP_NO` varchar(10) default null COMMENT '수정사번',
-  `UPD_ID` varchar(30) default null COMMENT '수정ID',
+  `UPD_EMP_NO` varchar(10) DEFAULT NULL COMMENT '수정사번',
+  `UPD_ID` varchar(30) DEFAULT NULL COMMENT '수정ID',
   `UPD_DATE` timestamp NULL DEFAULT current_timestamp() COMMENT '수정일자',
   PRIMARY KEY (`COMP_ID`,`RACK_CODE`,`RACK_NAME`,`RACK_DIV`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='창고위치';
