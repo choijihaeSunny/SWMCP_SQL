622283d1d75d4f990a27043a1e090a3d5fc0325a
choijihaeSunny <jhchoisunny@gmail.com>
2024-04-18 10:57:00 +0900

    금형 입고, 출고, 수리/수정, 폐기화면 테이블 DDL 생성

---
+++ DDL/CREATE_TABLE_TB_MOLD_INPUT_LOT.sql
@@ -0,0 +1,24 @@
+-- swmcp.tb_mold_input_lot definition
+
+CREATE TABLE `tb_mold_input_lot` (
+  `COMP_ID` varchar(10) NOT NULL COMMENT '사업장코드',
+  `SET_DATE` varchar(8) NOT NULL COMMENT '등록일자',
+  `SET_SEQ` varchar(4) NOT NULL COMMENT '등록순번',
+  `SET_NO` varchar(4) NOT NULL COMMENT 'NO',
+  `MOLD_INPUT_KEY` varchar(30) NOT NULL COMMENT '금형입고번호',
+  `MOLD_CODE` varchar(20) NOT NULL COMMENT '금형코드',
+  `LOT_NO` varchar(30) NOT NULL COMMENT 'LOT NO',
+  `RMK` varchar(100) DEFAULT NULL COMMENT '비고',
+  `TEMP1` varchar(10) DEFAULT NULL COMMENT '임시01',
+  `TEMP2` varchar(10) DEFAULT NULL COMMENT '임시02',
+  `TEMP3` varchar(10) DEFAULT NULL COMMENT '임시03',
+  `TEMP4` varchar(10) DEFAULT NULL COMMENT '임시04',
+  `TEMP5` varchar(10) DEFAULT NULL COMMENT '임시05',
+  `SYS_EMP_NO` varchar(10) NOT NULL COMMENT '등록사번',
+  `SYS_ID` varchar(30) NOT NULL COMMENT '등록ID',
+  `SYS_DATE` timestamp NULL DEFAULT current_timestamp() COMMENT '등록일자',
+  `UPD_EMP_NO` varchar(10) DEFAULT NULL COMMENT '수정사번',
+  `UPD_ID` varchar(30) DEFAULT NULL COMMENT '수정ID',
+  `UPD_DATE` timestamp NULL DEFAULT current_timestamp() COMMENT '수정일자',
+  PRIMARY KEY (`COMP_ID`,`MOLD_INPUT_KEY`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='금형입고관리';
---
+++ DDL/CREATE_TABLE_TB_MOLD_OUT.sql
@@ -0,0 +1,30 @@
+-- swmcp.tb_mold_out definition
+
+CREATE TABLE `tb_mold_out` (
+  `COMP_ID` varchar(10) NOT NULL COMMENT '사업장코드',
+  `SET_DATE` varchar(8) NOT NULL COMMENT '등록일자',
+  `SET_SEQ` varchar(4) NOT NULL COMMENT '등록순번',
+  `SET_NO` varchar(4) NOT NULL COMMENT 'NO',
+  `MOLD_OUT_KEY` varchar(30) NOT NULL COMMENT '금형출고번호',
+  `MOLD_CODE` varchar(20) NOT NULL COMMENT '금형코드',
+  `CUST_CODE` varchar(10) DEFAULT NULL COMMENT '거래처',
+  `QTY` decimal(10,0) DEFAULT 0 COMMENT '출고수량',
+  `LOT_NO` varchar(30) DEFAULT NULL COMMENT 'LOT NO',
+  `COST` decimal(16,4) DEFAULT 0.0000 COMMENT '단가',
+  `AMT` decimal(16,4) DEFAULT 0.0000 COMMENT '금액',
+  `EMP_NO` varchar(10) DEFAULT NULL COMMENT '담당자',
+  `DEPT_CODE` varchar(10) DEFAULT NULL COMMENT '담당부서',
+  `RMK` varchar(100) DEFAULT NULL COMMENT '비고',
+  `TEMP1` varchar(10) DEFAULT NULL COMMENT '임시01',
+  `TEMP2` varchar(10) DEFAULT NULL COMMENT '임시02',
+  `TEMP3` varchar(10) DEFAULT NULL COMMENT '임시03',
+  `TEMP4` varchar(10) DEFAULT NULL COMMENT '임시04',
+  `TEMP5` varchar(10) DEFAULT NULL COMMENT '임시05',
+  `SYS_EMP_NO` varchar(10) NOT NULL COMMENT '등록사번',
+  `SYS_ID` varchar(30) NOT NULL COMMENT '등록ID',
+  `SYS_DATE` timestamp NULL DEFAULT current_timestamp() COMMENT '등록일자',
+  `UPD_EMP_NO` varchar(10) DEFAULT NULL COMMENT '수정사번',
+  `UPD_ID` varchar(30) DEFAULT NULL COMMENT '수정ID',
+  `UPD_DATE` timestamp NULL DEFAULT current_timestamp() COMMENT '수정일자',
+  PRIMARY KEY (`COMP_ID`,`MOLD_OUT_KEY`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='금형출고관리';
