a16324a104d55b1fe5b2f8832151fa0bf1c22af7
choijihaeSunny <jhchoisunny@gmail.com>
2024-04-18 16:16:33 +0900

    기등록조회 SELECT 오류 수정

--- MOLD005/PKG_MOLD005$GET_MOLD_OUT_LIST_POP.sql
+++ MOLD005/PKG_MOLD005$GET_MOLD_OUT_LIST_POP.sql
@@ -1,19 +1,13 @@
 CREATE DEFINER=`root`@`%` PROCEDURE `swmcp`.`PKG_MOLD005$GET_MOLD_OUT_LIST_POP`(
-			IN A_SET_DATE 		TIMESTAMP,
-			IN A_SET_SEQ		varchar(4),
+			IN A_ST_DATE 			TIMESTAMP,
+			IN A_ED_DATE			TIMESTAMP,
             OUT N_RETURN      	INT,
             OUT V_RETURN      	VARCHAR(4000)
 )
 PROC:begin
 	
-	declare V_SET_SEQ varchar(3);
-	
 	declare exit HANDLER for sqlexception
 	call USP_SYS_GET_ERRORINFO_ALL(V_RETURN, N_RETURN); 
-
-	if trim(A_SET_SEQ) <> '' then
-		set V_SET_SEQ := LPAD(A_SET_SEQ, 3, '0');
-	end if;
 
 	select
 		  STR_TO_DATE(A.SET_DATE, '%Y%m%d') as SET_DATE,
