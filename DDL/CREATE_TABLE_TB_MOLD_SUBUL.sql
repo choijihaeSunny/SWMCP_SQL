78617e165e244e7e8a143745929656cd4cec3676
choijihaeSunny <jhchoisunny@gmail.com>
2024-04-22 10:48:15 +0900

    수불처리 프로시저 작성

---
+++ DDL/CREATE_TABLE_TB_MOLD_SUBUL.sql
@@ -0,0 +1,31 @@
+-- swmcp.tb_mold_subul definition
+
+CREATE TABLE `tb_mold_subul` (
+  `COMP_ID` varchar(10) NOT NULL COMMENT '사업장코드',
+  `KEY_VAL` varchar(100) NOT NULL COMMENT '수불KEY',
+  `IO_DATE` varchar(8) NOT NULL COMMENT '수불일자',
+  `IN_OUT` varchar(1) NOT NULL COMMENT '입/출고 (입:1, 출:2)',
+  `WARE_CODE` varchar(10) NOT NULL COMMENT '창고코드',
+  `MOLD_CODE` varchar(30) NOT NULL COMMENT '금형코드',
+  `LOT_NO` varchar(30) DEFAULT NULL COMMENT 'LOT NO',
+  `IO_GUBN` varchar(10) DEFAULT NULL COMMENT '입출내역구분',
+  `IO_QTY` decimal(16,4) DEFAULT 0.0000 COMMENT '수량/중량',
+  `IO_PRC` decimal(16,4) DEFAULT 0.0000 COMMENT '단가',
+  `IO_AMT` decimal(16,4) DEFAULT 0.0000 COMMENT '금액',
+  `TABLE_NAME` varchar(50) DEFAULT NULL COMMENT '테이블명',
+  `TABLE_KEY` varchar(100) DEFAULT NULL COMMENT '테이블KEY',
+  `STOCK_YN` varchar(1) DEFAULT NULL COMMENT '재고반영여부',
+  `CUST_CODE` varchar(10) DEFAULT NULL COMMENT '거래처',
+  `TEMP1` varchar(10) DEFAULT NULL COMMENT '임시01',
+  `TEMP2` varchar(10) DEFAULT NULL COMMENT '임시02',
+  `TEMP3` varchar(10) DEFAULT NULL COMMENT '임시03',
+  `TEMP4` varchar(10) DEFAULT NULL COMMENT '임시04',
+  `TEMP5` varchar(10) DEFAULT NULL COMMENT '임시05',
+  `SYS_EMP_NO` varchar(10) NOT NULL COMMENT '등록사번',
+  `SYS_ID` varchar(30) NOT NULL COMMENT '등록ID',
+  `SYS_DATE` timestamp NULL DEFAULT current_timestamp() COMMENT '등록일자',
+  `UPD_EMP_NO` varchar(10) DEFAULT NULL COMMENT '수정사번',
+  `UPD_ID` varchar(30) DEFAULT NULL COMMENT '수정ID',
+  `UPD_DATE` timestamp NULL DEFAULT current_timestamp() COMMENT '수정일자',
+  PRIMARY KEY (`COMP_ID`,`KEY_VAL`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='금형수불';
--- SP_MOLD_SUBUL_CREATE.sql
+++ SP_MOLD_SUBUL_CREATE.sql
@@ -1,13 +1,25 @@
 CREATE DEFINER=`ubidom`@`%` PROCEDURE `swmcp`.`SP_MOLD_SUBUL_CREATE`(
         IN A_COMP_ID VARCHAR(10),
-        IN A_KEY_VAL VARCHAR(100), -- 수불 KEY
+        IN A_KEY_VAL VARCHAR(100), -- 수불 key
         IN A_IN_OUT VARCHAR(1), -- 입고/출고 여부 입:1 출:2
+        IN A_WARE_CODE varchar(10),
+        IN A_LOT_NO varchar(30),
+        IN A_IO_GUBN varchar(10),
+        IN A_IO_QTY decimal(16,4),
+        IN A_IO_PRC decimal(16, 4),
+        IN A_IO_AMT decimal(16, 4),
+        IN A_TABLE_NAME varchar(50),
+        IN A_TABLE_KEY varchar(100),
+        IN A_STOCK_YN varchar(1),
+        IN A_CUST_CODE varchar(10),
+        IN A_STOCK_QTY decimal(10, 0),
+        IN A_WARE_POS varchar(10),
         
         IN A_SUBUL_YN varchar(1), -- 수불 발생 필요 여부?
         IN A_SAVE_DIV varchar(10), -- insert, update, delete 여부
         IN A_IO_DATE VARCHAR(8), -- 수불 발생 일자?
         IN A_STOCK_CHK	VARCHAR(1),
-        
+        IN A_MOLD_CODE varchar(30),
         IN A_SYS_ID decimal(10,0),
 		IN A_SYS_EMP_NO varchar(10),
 	
@@ -39,15 +51,17 @@ PROC_BODY : begin
 	if A_SUBUL_YN = 'Y' then
 		if A_SAVE_DIV = 'UPDATE' or A_SAVE_DIV = 'DELETE' then
 			-- 수정 또는 삭제할 경우 수불 생성
-			select N_OUT, WARE_CODE, MOLD_CODE, LOT_NO, STOCK_YN, IO_DATE, IO_QTY
-			into   V_IN_OUT, V_WARE_CODE, V_MOLD_CODE, V_LOT_NO, V_STOCK_YN, V_IO_DATE, V_IO_QTY
+			select N_OUT, WARE_CODE, MOLD_CODE, LOT_NO, STOCK_YN, 
+				   IO_DATE, IO_QTY
+			into   V_IN_OUT, V_WARE_CODE, V_MOLD_CODE, V_LOT_NO, V_STOCK_YN, 
+				   V_IO_DATE, V_IO_QTY
 			from   TB_MOLD_SUBUL 
 			where  COMP_ID = A_COMP_ID and KEY_VAL = A_KEY_VAL;
 		end if;
 	
 		-- INSERT, UPDATE, DELETE else if 구문
-		if A_SAVE_DIV = 'INSERT' then
-			-- 기존의 재고 최종 값을 구하는듯?
+		if A_SAVE_DIV = 'INSERT' then 
+			-- 기존의 재고값을 구하는듯?
 			select ifnull(SUM(CASE 
 								  WHEN IN_OUT = '1' THEN IO_QTY 
 								  ELSE IO_QTY * -1 
@@ -62,7 +76,13 @@ PROC_BODY : begin
 						   	   END), 0)
 			into   V_STOCK_QTY_PRE, -- 재고수량
 				   V_STOCK_QTY_PRE_DATE -- 스블일자
-			from   TB_MOLD_SUBUL;
+			from   TB_MOLD_SUBUL
+			where COMP_ID = A_COMP_ID
+			  and WARE_CODE = A_WARE_CODE
+			  and MOLD_CODE = A_MOLD_CODE
+			  and LOT_NO = A_LOT_NO
+			  and STOCK_YN = 'Y'
+			;
 		
 			if A_IN_OUT = '1' then -- 입고했을 경우
 				set V_IO_QTY_INOUT = A_IO_QTY;
@@ -79,7 +99,370 @@ PROC_BODY : begin
             	leave PROC_BODY;
             	/* SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT ='재고가 부족합니다.'; */  
             end if;	
+           
+            insert into TB_MOLD_SUBUL (
+            	COMP_ID, KEY_VAL, IO_DATE, IN_OUT, WARE_CODE, 
+	   			MOLD_CODE, LOT_NO, IO_GUBN, IO_QTY, IO_PRC, 
+	   			IO_AMT, TABLE_NAME, TABLE_KEY, STOCK_YN, CUST_CODE, 
+	  			SYS_EMP_NO, SYS_ID, SYS_DATE 
+            ) values (
+            	A_COMP_ID, A_KEY_VAL, A_IO_DATE, A_IN_OUT, A_WARE_CODE,
+            	A_MOLD_CODE, A_LOT_NO, A_IO_GUBN, A_IO_QTY, A_IO_PRC,
+            	A_IO_AMT, A_TABLE_NAME, A_TABLE_KEY, A_STOCK_YN, A_CUST_CODE,
+            	A_SYS_EMP_NO, A_SYS_ID, SYSDATE()
+            )
+            ;
 			
+            if A_STOCK_YN = 'Y' then -- 재고반영
+            	select COUNT(*)
+            	into V_CHK_CNT
+            	from TB_MOLD_STOCK
+            	where COMP_ID = A_COMP_ID
+            	  and WARE_CODE = A_WARE_CODE
+            	  and MOLD_CODE = A_MOLD_CODE
+            	  and LOT_NO = A_LOT_NO
+            	;
+            
+            	if V_CHK_CNT = 0 then
+            		insert into TB_MOLD_STOCK (
+            			COMP_ID, WARE_CODE, MOLD_CODE, LOT_NO, STOCK_QTY, 
+						CUST_CODE, WARE_POS, SYS_EMP_NO, SYS_DATE
+            		) values (
+            			A_COMP_ID, A_WARE_CODE, A_MOLD_CODE, A_LOT_NO, A_STOCK_QTY, 
+						A_CUST_CODE, A_WARE_POS, SYS_EMP_NO, SYSDATE()
+            		)
+            		;
+            	else
+            		update TB_MOLD_STOCK
+            		   set STOCK_QTY = V_STOCK_QTY,
+            		   	   UPD_EMP_NO = A_SYS_EMP_NO,
+            		   	   UPD_DATE = SYSDATE()
+            		  where COMP_ID = A_COMP_ID
+            		    and WARE_CODE = A_WARE_CODE
+            		    and MOLD_CODE = A_MOLD_CODE
+            		    and LOT_NO = A_LOT_NO
+            		  ;
+            	end if; -- if V_CHK_CNT = 0 then end
+            	
+            	-- 품목코드별 재고 처리
+            	-- TB_STOCK_ITEM 에 대응하는 MOLD 테이블을 모르겠어서 일단 보류.
+	
+            end if; -- if A_STOCK_YN = 'Y' then end
+            
+        elseif A_SAVE_DIV = 'UPDATE' then 
+        	
+        	if 
+	        	   A_IN_OUT <> V_IN_OUT	
+	        	or A_WARE_CODE <> V_WARE_CODE
+	        	or A_MOLD_CODE <> V_MOLD_CODE
+	        	or A_LOT_NO <> V_LOT_NO
+	        	or A_STOCK_YN <> V_STOCK_YN
+	        then
+        		if V_STOCK_YN = 'Y' then -- 변경 전 정보
+        			select ifnull(SUM(CASE 
+										  WHEN IN_OUT = '1' THEN IO_QTY 
+										  ELSE IO_QTY * -1 
+									  END), 0),
+						   ifnull(SUM(CASE 
+							   			  WHEN IO_DATE <= A_IO_DATE THEN 
+							   			  								CASE 
+								   			  								WHEN IN_OUT = '1' THEN IO_QTY 
+								   			  												  ELSE IO_QTY * -1 
+								   			  							END 
+								   		  ELSE 0 
+								   	   END), 0)
+					 into   V_STOCK_QTY_PRE, -- 재고수량
+						    V_STOCK_QTY_PRE_DATE -- 스블일자
+					 from   TB_MOLD_SUBUL
+					 where COMP_ID = V_COMP_ID
+					   and WARE_CODE = V_WARE_CODE
+					   and MOLD_CODE = V_MOLD_CODE
+					   and LOT_NO = V_LOT_NO
+					   and STOCK_YN = 'Y'
+					;
+					
+					 if A_IN_OUT = '1' then -- 입고했을 경우
+						 set V_IO_QTY_INOUT = V_IO_QTY * -1;
+					 else -- 출고했을 경우
+						 set V_IO_QTY_INOUT = V_IO_QTY;
+					 end if;
+					
+					 set V_STOCK_QTY = V_STOCK_QTY_PRE + V_IO_QTY_INOUT;
+					 set V_STOCK_QTY_DATE = V_STOCK_QTY_PRE_DATE + V_IO_QTY_INOUT;
+					
+					 if V_STOCK_QTY_DATE < 0 and A_STOCK_CHK = 'Y' then
+		            	 SET N_RETURN = -1;
+		  				 SET V_RETURN = '[UPDATE] 변경전 재고가 부족합니다.'; 
+		            	 leave PROC_BODY;
+		            	 /* SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT ='재고가 부족합니다.'; */  
+		             end if;
+		            
+		             select COUNT(*)
+	            	   into V_CHK_CNT
+	            	 from TB_MOLD_STOCK
+	            	 where COMP_ID = A_COMP_ID
+	            	   and WARE_CODE = A_WARE_CODE
+	            	   and MOLD_CODE = A_MOLD_CODE
+	            	   and LOT_NO = A_LOT_NO
+	            	 ;
+	            	
+	            	 if V_CHK_CNT = 0 then
+	            	 	insert into TB_MOLD_STOCK (
+	            			COMP_ID, WARE_CODE, MOLD_CODE, LOT_NO, STOCK_QTY, 
+							CUST_CODE, WARE_POS, SYS_EMP_NO, SYS_DATE
+	            		) values (
+	            			A_COMP_ID, A_WARE_CODE, A_MOLD_CODE, A_LOT_NO, A_STOCK_QTY, 
+							A_CUST_CODE, A_WARE_POS, SYS_EMP_NO, SYSDATE()
+	            		)
+	            		;
+	            	 else
+	            	 	update TB_MOLD_STOCK
+	            		   set STOCK_QTY = V_STOCK_QTY,
+	            		   	   UPD_EMP_NO = A_SYS_EMP_NO,
+	            		   	   UPD_DATE = SYSDATE()
+	            		 where COMP_ID = A_COMP_ID
+	            		   and WARE_CODE = A_WARE_CODE
+	            		   and MOLD_CODE = A_MOLD_CODE
+	            		   and LOT_NO = A_LOT_NO
+	            		 ;
+	            	 end if; -- if V_CHK_CNT = 0 then end
+		            
+	            	 -- 품목코드별 재고 처리
+            		 -- TB_STOCK_ITEM 에 대응하는 MOLD 테이블을 모르겠어서 일단 보류.
+					 
+        		end if; -- if V_STOCK_YN = 'Y' then end
+        		
+        		if A_STOCK_YN = 'Y' then
+        			select ifnull(SUM(CASE 
+										  WHEN IN_OUT = '1' THEN IO_QTY 
+										  ELSE IO_QTY * -1 
+									  END), 0),
+						   ifnull(SUM(CASE 
+							   			  WHEN IO_DATE <= A_IO_DATE THEN 
+							   			  								CASE 
+								   			  								WHEN IN_OUT = '1' THEN IO_QTY 
+								   			  												  ELSE IO_QTY * -1 
+								   			  							END 
+								   		  ELSE 0 
+								   	   END), 0)
+					 into   V_STOCK_QTY_PRE, -- 재고수량
+						    V_STOCK_QTY_PRE_DATE -- 스블일자
+					 from   TB_MOLD_SUBUL
+					 where COMP_ID = A_COMP_ID
+					   and WARE_CODE = A_WARE_CODE
+					   and MOLD_CODE = A_MOLD_CODE
+					   and LOT_NO = A_LOT_NO
+					   and STOCK_YN = 'Y'
+					 ;
+					
+					 if A_IN_OUT = '1' then
+					 	set V_IO_QTY_INOUT = A_IO_QTY;
+					 else
+					 	set V_IO_QTY_INOUT = A_IO_QTY * -1;
+					 end if;
+					
+					 set V_STOCK_QTY = V_STOCK_QTY_PRE + V_IO_QTY_INOUT;
+					 set V_STOCK_QTY_DATE = V_STOCK_QTY_PRE_DATE + V_IO_QTY_INOUT;
+					
+					 if V_STOCK_QTY_DATE < 0 and A_STOCK_CHK = 'Y' then
+		            	 SET N_RETURN = -1;
+		  				 SET V_RETURN = '[UPDATE] 변경후 재고가 부족합니다.'; 
+		            	 leave PROC_BODY;
+		            	 /* SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT ='재고가 부족합니다.'; */  
+		             end if;
+		            
+		             select COUNT(*)
+	            	   into V_CHK_CNT
+	            	 from TB_MOLD_STOCK
+	            	 where COMP_ID = A_COMP_ID
+	            	   and WARE_CODE = A_WARE_CODE
+	            	   and MOLD_CODE = A_MOLD_CODE
+	            	   and LOT_NO = A_LOT_NO
+	            	 ;
+	            	
+	            	 if V_CHK_CNT = 0 then
+	            	 	insert into TB_MOLD_STOCK (
+	            			COMP_ID, WARE_CODE, MOLD_CODE, LOT_NO, STOCK_QTY, 
+							CUST_CODE, WARE_POS, SYS_EMP_NO, SYS_DATE
+	            		) values (
+	            			A_COMP_ID, A_WARE_CODE, A_MOLD_CODE, A_LOT_NO, A_STOCK_QTY, 
+							A_CUST_CODE, A_WARE_POS, SYS_EMP_NO, SYSDATE()
+	            		)
+	            		;
+	            	 else
+	            	 	update TB_MOLD_STOCK
+	            		   set STOCK_QTY = V_STOCK_QTY,
+	            		   	   UPD_EMP_NO = A_SYS_EMP_NO,
+	            		   	   UPD_DATE = SYSDATE()
+	            		 where COMP_ID = A_COMP_ID
+	            		   and WARE_CODE = A_WARE_CODE
+	            		   and MOLD_CODE = A_MOLD_CODE
+	            		   and LOT_NO = A_LOT_NO
+	            		 ;
+	            	 end if; -- if V_CHK_CNT = 0 then end
+	            	 
+	            	 -- 품목코드별 재고 처리
+            		 -- TB_STOCK_ITEM 에 대응하는 MOLD 테이블을 모르겠어서 일단 보류.
+	            	 
+        		end if; -- if A_STOCK_YN = 'Y' then end
+        	else
+        		if A_STOCK_YN = 'Y' then
+        			select ifnull(SUM(CASE 
+										  WHEN IN_OUT = '1' THEN IO_QTY 
+										  ELSE IO_QTY * -1 
+									  END), 0),
+						   ifnull(SUM(CASE 
+							   			  WHEN IO_DATE <= A_IO_DATE THEN 
+							   			  								CASE 
+								   			  								WHEN IN_OUT = '1' THEN IO_QTY 
+								   			  												  ELSE IO_QTY * -1 
+								   			  							END 
+								   		  ELSE 0 
+								   	   END), 0)
+					 into   V_STOCK_QTY_PRE, -- 재고수량
+						    V_STOCK_QTY_PRE_DATE -- 스블일자
+					 from   TB_MOLD_SUBUL
+					 where COMP_ID = A_COMP_ID
+					   and WARE_CODE = A_WARE_CODE
+					   and MOLD_CODE = A_MOLD_CODE
+					   and LOT_NO = A_LOT_NO
+					   and STOCK_YN = 'Y'
+					 ;
+					
+					 if A_IN_OUT = '1' then
+					 	set V_IO_QTY_INOUT = A_IO_QTY;
+					 else
+					 	set V_IO_QTY_INOUT = A_IO_QTY * -1;
+					 end if;
+					
+					 set V_STOCK_QTY = V_STOCK_QTY_PRE + V_IO_QTY_INOUT;
+					 set V_STOCK_QTY_DATE = V_STOCK_QTY_PRE_DATE + V_IO_QTY_INOUT;
+					
+					 if V_STOCK_QTY_DATE < 0 and A_STOCK_CHK = 'Y' then
+		            	 SET N_RETURN = -1;
+		  				 SET V_RETURN = '[UPDATE] 변경후 재고가 부족합니다.'; 
+		            	 leave PROC_BODY;
+		            	 /* SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT ='재고가 부족합니다.'; */  
+		             end if;
+		            
+		             select COUNT(*)
+	            	   into V_CHK_CNT
+	            	 from TB_MOLD_STOCK
+	            	 where COMP_ID = A_COMP_ID
+	            	   and WARE_CODE = A_WARE_CODE
+	            	   and MOLD_CODE = A_MOLD_CODE
+	            	   and LOT_NO = A_LOT_NO
+	            	 ;
+	            	
+	            	 if V_CHK_CNT = 0 then
+	            	 	insert into TB_MOLD_STOCK (
+	            			COMP_ID, WARE_CODE, MOLD_CODE, LOT_NO, STOCK_QTY, 
+							CUST_CODE, WARE_POS, SYS_EMP_NO, SYS_DATE
+	            		) values (
+	            			A_COMP_ID, A_WARE_CODE, A_MOLD_CODE, A_LOT_NO, A_STOCK_QTY, 
+							A_CUST_CODE, A_WARE_POS, SYS_EMP_NO, SYSDATE()
+	            		)
+	            		;
+	            	 else
+	            	 	update TB_MOLD_STOCK
+	            		   set STOCK_QTY = V_STOCK_QTY,
+	            		   	   UPD_EMP_NO = A_SYS_EMP_NO,
+	            		   	   UPD_DATE = SYSDATE()
+	            		 where COMP_ID = A_COMP_ID
+	            		   and WARE_CODE = A_WARE_CODE
+	            		   and MOLD_CODE = A_MOLD_CODE
+	            		   and LOT_NO = A_LOT_NO
+	            		 ;
+	            	 end if; -- if V_CHK_CNT = 0 then end
+	            	 
+	            	 -- 품목코드별 재고 처리
+            		 -- TB_STOCK_ITEM 에 대응하는 MOLD 테이블을 모르겠어서 일단 보류.
+	            	 
+        		end if; -- if A_STOCK_YN = 'Y' then end
+        	end if;
+        
+        	update TB_MOLD_SUBUL
+        	   set IO_DATE = A_IO_DATE,
+        	   	   IN_OUT = A_IN_OUT,
+        	   	   WARE_CODE = A_WARE_CODE,
+        	   	   MOLD_CODE = A_MOLD_CODE,
+        	   	   LOT_NO = A_LOT_NO,
+        	   	   STOCK_YN = A_STOCK_YN,
+        	   	   IO_QTY = A_IO_QTY,
+        	   	   IO_PRC = A_IO_PRC,
+        	   	   IO_AMT = A_IO_AMT,
+        	   	   CUST_CODE = A_CUST_CODE,
+        	   	   UPD_EMP_NO = A_SYS_EMP_NO,
+        	   	   UPD_DATE = SYSDATE()
+        	 where COMP_ID = A_COMP_ID
+        	   and KEY_VAL = A_KEY_VAL
+        	;
+        
+        elseif A_SAVE_DIV = 'DELETE' then
+        	
+        	if V_STOCK_YN = 'Y' then
+        		select ifnull(SUM(CASE 
+									  WHEN IN_OUT = '1' THEN IO_QTY 
+									  ELSE IO_QTY * -1 
+								  END), 0),
+					   ifnull(SUM(CASE 
+						   			  WHEN IO_DATE <= A_IO_DATE THEN 
+						   			  								CASE 
+							   			  								WHEN IN_OUT = '1' THEN IO_QTY 
+							   			  												  ELSE IO_QTY * -1 
+							   			  							END 
+							   		  ELSE 0 
+							   	   END), 0)
+				into   V_STOCK_QTY_PRE, -- 재고수량
+					   V_STOCK_QTY_PRE_DATE -- 스블일자
+				from   TB_MOLD_SUBUL
+				where COMP_ID = A_COMP_ID
+				  and WARE_CODE = A_WARE_CODE
+				  and MOLD_CODE = A_MOLD_CODE
+				  and LOT_NO = A_LOT_NO
+				;
+			
+				if A_IN_OUT = '1' then 
+					set V_IO_QTY_INOUT = V_IO_QTY * -1;
+				else
+					set V_IO_QTY_INOUT = V_IO_QTY;
+				end if;
+			
+				if V_STOCK_QTY_DATE < 0 and A_STOCK_CHK = 'Y' then
+	            	SET N_RETURN = -1;
+	  				SET V_RETURN = '[DELETE] 재고가 부족합니다.'; 
+	            	leave PROC_BODY;
+	            	/* SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT ='재고가 부족합니다.'; */  
+	            end if;	
+	           
+	           	if V_CHK_CNT = 0 then
+            		insert into TB_MOLD_STOCK (
+            			COMP_ID, WARE_CODE, MOLD_CODE, LOT_NO, STOCK_QTY, 
+						CUST_CODE, WARE_POS, SYS_EMP_NO, SYS_DATE
+            		) values (
+            			A_COMP_ID, A_WARE_CODE, A_MOLD_CODE, A_LOT_NO, A_STOCK_QTY, 
+						A_CUST_CODE, A_WARE_POS, SYS_EMP_NO, SYSDATE()
+            		)
+            		;
+            	else
+            		update TB_MOLD_STOCK
+            		   set STOCK_QTY = V_STOCK_QTY,
+            		   	   UPD_EMP_NO = A_SYS_EMP_NO,
+            		   	   UPD_DATE = SYSDATE()
+            		  where COMP_ID = A_COMP_ID
+            		    and WARE_CODE = A_WARE_CODE
+            		    and MOLD_CODE = A_MOLD_CODE
+            		    and LOT_NO = A_LOT_NO
+            		  ;
+            	end if; -- if V_CHK_CNT = 0 then end
+            	
+        	end if; -- if V_STOCK_YN = 'Y' then
+        	
+        	delete from TB_MOLD_SUBUL
+        	where COMP_ID = A_COMP_ID
+        	  and KEY_VAL = A_KEY_VAL
+        	;
+        	
 		end if; -- INSERT, UPDATE, DELETE else if 구문 end
 	end if; -- if A_SUBUL_YN = 'Y' then endif
 
