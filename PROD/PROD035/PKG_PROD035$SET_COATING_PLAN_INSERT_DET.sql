6dbd2bb4810d177a99ab2c06b4fec3a1060dbf5d
choijihaeSunny <jhchoisunny@gmail.com>
2024-06-11 10:43:00 +0900

    원자재코팅 INSERT 수정

--- PROD/PROD035/PKG_PROD035$SET_COATING_PLAN_INSERT_DET.sql
+++ PROD/PROD035/PKG_PROD035$SET_COATING_PLAN_INSERT_DET.sql
@@ -1,7 +1,7 @@
 CREATE DEFINER=`root`@`%` PROCEDURE `swmcp`.`PKG_PROD035$SET_COATING_PLAN_INSERT_DET`(	
 	IN A_COMP_ID varchar(10),
 	IN A_MATR_LOT_NO varchar(30),
-	IN A_SET_SEQ varchar(2),
+#	IN A_SET_SEQ varchar(2),
 	IN A_PLAN_DATE varchar(8),
 	IN A_PLAN_MST_KEY varchar(50),
 	IN A_ORDER_KEY		varchar(30),
@@ -23,6 +23,7 @@ begin
 	declare V_WORK_PLAN_KEY VARCHAR(50);
 	declare V_TOT_QTY DECIMAL;
 	declare V_PLAN_QTY2 DECIMAL;
+	declare V_SET_SEQ varchar(2);
 
 	DECLARE EXIT HANDLER FOR SQLEXCEPTION 
 	CALL USP_SYS_GET_ERRORINFO_ALL(V_RETURN, N_RETURN); 
@@ -30,8 +31,12 @@ begin
 	SET N_RETURN = 0;
   	SET V_RETURN = '저장되었습니다.'; 
   
-	set V_WORK_PLAN_KEY = CONCAT(A_PLAN_MST_KEY, DATE_FORMAT(A_PLAN_DATE, '%Y%m%d')); -- 생산계획 KEY (계획MST KEY + 계획일자)
+	set V_WORK_PLAN_KEY = CONCAT(A_PLAN_MST_KEY, A_PLAN_DATE); -- 생산계획 KEY (계획MST KEY + 계획일자)
   	
+	set V_SET_SEQ = (select LPAD(IFNULL(MAX(SET_SEQ), 0) + 1, 2, '0')
+					 from TB_COATING_PLAN_DET
+					 where PLAN_DATE = A_PLAN_DATE
+					   and PLAN_MST_KEY = A_PLAN_MST_KEY);
     
 	insert into TB_COATING_PLAN_DET (
 		COMP_ID,
@@ -45,9 +50,7 @@ begin
 		PROG_CODE,
 		PLAN_QTY,
 		DEPT_CODE,
-		CONFIRM_YN,
 		WORK_QTY,
-		END_YN,
 		RMK,
 		SYS_ID,
 		SYS_EMP_NO,
